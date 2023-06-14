58f5510684b5d0c6279451192fe75305671d4569
Stephen Zhou <hi@hyoban.cc>
2023-06-14 18:13:18 +0800

    feat: use gpt-3.5-turbo-0613

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
 const openAIKey = Deno.env.get("OPENAI_KEY");
 const feishuBotId = Deno.env.get("FEISHU_BOT_ID");
 
+interface Notification {
+  project?: string;
+  time?: string;
+  status?: string;
+  author?: string;
+  message?: string;
+}
+
+function constructNotificationText(content: Notification) {
+  const { project, time, status, author, message } = content;
+  const notificationText = `项目 ${project} 在 ${time} 时 ${status}，提交者为 ${author}，提交信息为 ${message}`;
+  return notificationText;
+}
+
 async function requestOpenAI(prompt: string) {
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
@@ -9,18 +23,75 @@ async function requestOpenAI(prompt: string) {
     },
     method: "POST",
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-0613",
       messages: [
         {
           role: "user",
-          content: `你是一个webhook机器人，接收一个json字符串作为输入，其内容一般为代码的编译部署进度状态信息或其他通知信息。你需要获取输入全部的信息，将其转换为一条便于人类阅读了解的通知，内容尽可能丰富，作为回复。注意输出应该为一行内容，不要换行。以下是输入的json：${prompt}`,
+          content: `The following is a json object that contains the information of a project's notification: ${prompt}`,
+        },
+      ],
+      functions: [
+        {
+          name: "constructNotificationText",
+          description: "Construct a notification text from a json object",
+          parameters: {
+            type: "object",
+            properties: {
+              project: {
+                type: "string",
+                description: "The name of the project",
+              },
+              time: {
+                type: "string",
+                description: "The time of the notification",
+              },
+              status: {
+                type: "string",
+                description: "The new status of the project",
+              },
+              author: {
+                type: "string",
+                description: "The author of the commit",
+              },
+              message: {
+                type: "string",
+                description: "The message of the commit",
+              },
+            },
+          },
         },
       ],
     }),
   });
 
-  const data = await res.json();
-  return data.choices[0].message.content;
+  const data = (await res.json()) as OpenAIResponse;
+  const argument = JSON.parse(
+    data.choices[0].message.function_call.arguments
+  ) as Notification;
+  const message = constructNotificationText(argument);
+  return message;
+}
+
+export interface OpenAIResponse {
+  id: string;
+  choices: Choice[];
+}
+
+export interface Choice {
+  index: number;
+  message: Message;
+  finish_reason: string;
+}
+
+export interface Message {
+  role: string;
+  content: any;
+  function_call: FunctionCall;
+}
+
+export interface FunctionCall {
+  name: string;
+  arguments: string;
 }
 
 async function sendToFeishu(message: string) {
